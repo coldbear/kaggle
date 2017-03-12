3233a77f01175fbc8482ca0e5731dc814c32cc7c
coldbear <coldbear@users.noreply.github.com>
2017-03-12 16:09:40 +0100

    Hello!

---
+++ geo.R
@@ -0,0 +1,26 @@
+dato$street_number=0
+dato$route=0
+dato$neighborhood <- 0
+dato$political=0
+for (i in 1:length(batch1000$bathrooms)){
+  res <- revgeocode(c(batch1000$longitude[i], batch1000$latitude[i]), output="more")
+  dato$street_number[i]=levels(res$street_number)
+  dato$route[i]=levels(res$route)
+  dato$neighborhood[i]=levels(res$neighborhood)
+  dato$political[i]=levels(res$political)
+}
+q=161
+q2=1000
+trailbatch <-  dato[q:q2,]
+batch4000 <- dato[1001:4000,]
+for (i in q:q2){
+  res <- revgeocode(c(trailbatch$longitude[i], trailbatch$latitude[i]), output="more")
+  if(is.null(levels(res$street_number))==TRUE){dato$street_number[i]=NA}
+  else {dato$street_number[i]=levels(res$street_number)}
+  if(is.null(levels(res$route))==TRUE){dato$route[i]=NA}
+  else {dato$route[i]=levels(res$route)}
+  if(is.null(levels(res$political))==TRUE){dato$political[i]=NA}
+  else {dato$political[i]=levels(res$political)}
+  if(is.null(levels(res$neighborhood))==TRUE){dato$neighborhood[i]=NA}
+  else {dato$neighborhood[i]=levels(res$neighborhood)}
+}
